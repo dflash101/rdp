fa2b7f936eb3804852bca31fed1c0396178f0f7c
dflash101 <115047230+dflash101@users.noreply.github.com>
2025-03-27 12:32:35 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,20 +1,21 @@
 @echo off
-
-:: Change the built-in Administrator password (no /add)
-net user administrator OLDUSER#6 >nul
-net localgroup administrators administrator >nul
-net user administrator /active:yes >nul
-
-:: Example: Retrieve the Ngrok tunnel URL using PowerShell
-tasklist | find /i "ngrok.exe" >Nul && powershell -Command ^
-  "$url = (Invoke-WebRequest -UseBasicParsing http://localhost:4040/api/tunnels | ConvertFrom-Json).tunnels[0].public_url; ^
-   if ($url) {echo $url} else {echo 'Failed to retrieve NGROK URL'}" ^
-  || echo "Ngrok not running or can't retrieve URL"
-
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By administrator " > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator OLDUSER#6 /add >nul
+net localgroup administrators  administrator  /add >nul
+net user administrator  /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator :F >nul
+ICACLS C:\Windows\installer /grant administrator :F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
-echo Username: administrator
+tasklist | find /i "ngrok/ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: administrator 
 echo Password: OLDUSER#6
 echo You can login now
-
 ping -n 10 127.0.0.1 >nul
